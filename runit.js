878ecc6cd051fbead1cca8be6f821e9ca3650abd
manny2384 <gonzalezeh2015@gmail.com>
2020-02-28 14:35:02 -0800

    New Site

---
+++ runit.js
@@ -0,0 +1,18 @@
+const nunjucks = require('nunjucks');
+const fs = require('fs');  // The file system module
+
+//list of files
+let files = ["index.html", "about.html", "login.html", "activities.html","members.html"]
+
+let srcDir ="./content/";
+let outDir="./output/";
+
+// Tells nunjucks where to look for templates and set any options
+nunjucks.configure('views', { autoescape: false });
+
+for (let x of files){
+	let contents = fs.readFileSync(srcDir + x);
+	let outString = nunjucks.render('base.njk', {mainContent: contents});
+	fs.writeFileSync(outDir + x, outString);
+	console.log("Wrote file" + x);
+}
